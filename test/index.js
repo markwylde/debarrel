4137a07fe6a0e57ee0770d0a9101f7f9acaf9acf
Mark <me@markwylde.com>
2020-12-04 21:43:26 +1000

    Fix tests

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ const debarrel = require('../');
 test('flushes after maximum flush time', t => {
   t.plan(1);
 
-  let timer;
+  let timer = null;
   let total = 0;
   const watch = debarrel(function () {
     clearInterval(timer);
@@ -22,4 +22,29 @@ test('flushes after maximum flush time', t => {
   timer = setInterval(() => {
     increment(1);
   }, 20);
+});
+
+test('does not flush until minimum flush time', t => {
+  t.plan(3);
+
+  const timer = null;
+  let total = 0;
+
+  const watch = debarrel(function () {
+    const timeTaken = Date.now() - startTime;
+    t.ok(timeTaken >= 90, `flushed after 50ms (${timeTaken})`);
+    t.ok(timeTaken < 200, `flushed before 100ms (${timeTaken})`);
+    clearInterval(timer);
+    t.equal(total, 1);
+  }, {
+    minimumFlushTime: 100,
+    maximumFlushTime: 500
+  });
+
+  const startTime = Date.now();
+  const increment = watch(function (increment) {
+    total = total + increment;
+  });
+
+  increment(1);
 });
